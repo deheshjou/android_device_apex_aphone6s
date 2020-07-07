thinhx2/android_device_volla_yggdrasil-twrp

add TARGET_COPY_OUT_VENDOR

BoardConfig.mk
@@ -52,6 +52,7 @@ BOARD_CACHEIMAGE_PARTITION_SIZE := 218103808
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_VENDORIMAGE_PARTITION_SIZE := 897581056
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR := v
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true

deheshjou/android_device_apex_aphone6s

BoardConfig.mk
@@ -52,6 +52,7 @@ BOARD_CACHEIMAGE_PARTITION_SIZE := 218103808
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_VENDORIMAGE_PARTITION_SIZE := 897581056
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR := vendor
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true