95b2c8bc29f95860c259fbaabafa8d49f29a35d7
Erfan Abdi <erfangplus@gmail.com>
2020-07-08 22:11:07 +0430

    aphone6s: Set better DPI

--- product_prop.mk
+++ product_prop.mk
@@ -24,7 +24,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # LCD Density
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.sf.lcd_density=560
+    ro.sf.lcd_density=420
 
 # Provides overrides to configure the Dalvik heap for a 4GB phone
 PRODUCT_PRODUCT_PROPERTIES += \
